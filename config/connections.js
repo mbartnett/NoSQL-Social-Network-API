ffe33e162d73951c2e8909899b6e5e84aa03a687
Mike B <115735557+mbartnett@users.noreply.github.com>
2023-03-08 17:13:25 -0600

    updating pre-tutoring

--- config/connections.js
+++ config/connections.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost:27017/nosql-social-network-api",
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/nosql-social-network-api",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
